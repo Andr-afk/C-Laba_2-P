48c9a8d402c6a732ac0f95a6646e2e4a64ba2b4c
Andr-afk <56882356+Andr-afk@users.noreply.github.com>
2020-09-29 13:03:45 +0700

    first commit

---
+++ PNumber/PNumber.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PNumber
+{
+    class PNumber
+    {
+        string value;
+        int base_value;
+        int accuracy;
+
+        public PNumber(string value, string base_value, string accuracy)
+        {
+            this.value = value;
+            this.base_value = checkInputData(base_value);
+            this.accuracy = checkInputData(accuracy);
+
+
+            if (this.base_value > 16 || this.base_value < 2) throw new Exception("Недопустимый диаппозон основания числа");
+        }
+
+        public PNumber(int value, int base_value, int accuracy)
+        {
+            this.value = Convert.ToString(value);
+            this.base_value = base_value;
+            this.accuracy = accuracy;
+
+            if (this.base_value > 16 || this.base_value < 2) throw new Exception("Недопустимый диаппозон основания числа");
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}({1})", this.value, this.base_value);
+        }
+
+        private static int checkInputData(string input_value)
+        {
+            int outputValue;
+            if (!int.TryParse(input_value, out outputValue))
+            {
+                throw new Exception("Недопустимый формат данных");
+            }
+
+            return outputValue;
+        }
+
+    }
+}
---
+++ PNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PNumber test = new PNumber("4564", "4", "0");
+            Console.WriteLine(test);
+        }
+    }
+}
